78c87823749bf7b51be594a88e321d326c4ed873
Daniel Nazarian <danielnazarian@outlook.com>
2021-12-21 15:29:40 -0500

    got python/pyenv good

--- language-scripts/python.sh
+++ language-scripts/python.sh
@@ -11,10 +11,7 @@ brew update
 brew install python
 
 # default to python 3
-echo "# python config" >> ~/.zshrc
-echo "alias python=python3" >> ~/.zshrc
-echo "" >> ~/.zshrc
-source ~/.zshrc
+# TODO
 
 # install pyenv
 brew install pyenv
@@ -27,14 +24,25 @@ echo "export PATH="$PYENV_ROOT/bin:$PATH"" >> ~/.zprofile
 echo "" >> ~/.zprofile
 echo "# pyenv config" >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+echo 'export PYENV_ROOT=$(pyenv root)' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/shims:$PATH"' >> ~/.zshrc
+echo "alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'" >> ~/.zshrc  # fix for brew warning
+echo "if command -v pyenv 1>/dev/null 2>&1; then" >> ~/.zshrc 
+echo "    eval "$(pyenv init -)"" >> ~/.zshrc 
+echo "fi" >> ~/.zshrc 
 echo "" >> ~/.zshrc
-alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'  # fix for brew warning
 source ~/.zprofile
 source ~/.zshrc
 
 # download most recent version and set
 pyenv install $(pyenv install --list | grep --extended-regexp "^\s*[0-9][0-9.]*[0-9]\s*$" | tail -1)
-pyenv global system
+pyenv global $(pyenv install --list | grep --extended-regexp "^\s*[0-9][0-9.]*[0-9]\s*$" | tail -1)
+
+# install virtualenv
+brew install pyenv-virtualenv
+
+# auto activate virtualenv
+echo ‘eval “$(pyenv virtualenv-init -)”’ >> ~/.zshrc
 
 # install pipenv
 pip3 install --user pipenv
@@ -44,7 +52,12 @@ echo "# pipenv config" >> ~/.zprofile
 echo "export PYTHON_BIN_PATH="$(python3 -m site --user-base)/bin"" >> ~/.zprofile
 echo "export PATH="$PATH:$PYTHON_BIN_PATH" " >> ~/.zprofile
 echo "" >> ~/.zprofile
+echo "# pipenv config" >> ~/.zshrc
+echo "path+=("$HOME/.local/bin")" >> ~/.zshrc
+echo "export PATH" >> ~/.zshrc
+echo "" >> ~/.zshrc
 source ~/.zprofile
+source ~/.zshrc
 
 # brew doctor
 brew doctor -v
