07ef65e3cc62fbbb884a3fab4423b36f94fe7285
Daniel Nazarian <danielnazarian@outlook.com>
2021-12-21 14:04:35 -0500

    brew and git script work

--- brew.sh
+++ brew.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Homebrew setup script"
+echo ""
+echo "Installs homebrew, config and major necessary packages."
+echo ""
 
 # install brew
 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
@@ -6,9 +10,16 @@ $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 
 # install major brew packages
 brew install gh
-brew install heroku
-    - brew install gh 
-    - brew install heroku
-    - brew install postgres?
+brew tap heroku/brew && brew install heroku
+brew install postgres
 
 # brew doctor
+brew doctor -v
+
+
+# final message
+echo "**"
+echo "Please complete as much of the output of the above as you can to ensure your"
+echo "Homebrew installation is as solid as possible!"
+echo ""
+echo "**"
--- git.sh
+++ git.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
 
-brew global config
-- git config --global user.email "danielnazarian@outlook.com"
-- git config --global user.name "Daniel Nazarian"
+# git global config
+git config --global user.email "danielnazarian@outlook.com"
+git config --global user.name "Daniel Nazarian"
 
 
-setup github cli?
+# setup heroku cli
 
-setup heroku cli?
+
+# setup github cli
+
